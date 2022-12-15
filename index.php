f5f227e289318d28ab060151ef474aa704b7357a
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-12-15 17:09:14 +0100

    Aggiunge layout e array lettere, simboli e numeri

--- index.php
+++ index.php
@@ -18,6 +18,24 @@
 // Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
 // Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
 
+$alphabet = [ array_merge(range('A', 'Z'), range('a', 'z')) ];
+
+foreach( $alphabet as $letters) {
+    echo $letters . ", ";
+}
+
+$numbers = [ range('0', '9') ];
+
+foreach( $numbers as $numbers) {
+    echo $numbers . ", ";
+}
+
+$special_character = ['!','@','#','$','%','&','*','_','+','=','/','-','','<','>','?',];
+
+var_dump($letters);
+var_dump($numbers);
+var_dump($special_character);
+
 ?>
 
 <!DOCTYPE html>
@@ -27,8 +45,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Strong Password Generator</title>
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- /Bootstrap CDN -->
 </head>
 <body>
-    
+    <div class="container">
+        <h1>Strong Password Generator</h1>
+        <h2>Genera una password sicura</h2>
+        <form action="index.php" method="GET">
+            <div class="mb-3">
+                <label for="password-length">Lunghezza della password</label>
+                <input type="number" name="pw_length" id="password-length" minlength="8" maxlength="24" required>
+            </div>
+            <div class="mb-3">
+                <button class="btn btn-primary">Invia</button>
+            </div>
+        </form>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!-- /Bootstrap JS -->
 </body>
 </html>
